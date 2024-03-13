913117f6986bfe9c29282aa383d9b03a8fd20644
Naomi Zhong <xoxizhong@gmail.com>
2024-03-12 21:57:30 -0400

    week-1-practices

---
+++ week-1/practice-conditional.js
@@ -0,0 +1,39 @@
+// a short program to determine the output according to the age input.
+var age = 10;
+if (age >= 65) {
+    console.log("You get your income from your pension")
+} else if (age < 65 && age >= 18) {
+    console.log("Each month you get a salary")
+} else if (age < 18) {
+    console.log("You get an allowance")
+} else {
+    console.log("The value of the age variable is not numerical")
+}
+
+
+
+// a short program using switch statement for different day of th week
+var day = "Sunday";
+switch (day) {
+    case 'Monday':
+        console.log("Do something");
+        break;
+    case 'Tuesday':
+        console.log("Do something");
+        break;
+    case 'Wednesday':
+        console.log("Do something");
+        break;
+    case 'Thursday':
+        console.log("Do something");
+        break;
+    case 'Friday':
+        console.log("Do something");
+        break;
+    case 'Saturday':
+        console.log("Do something");
+        break;
+    case 'Sunday':
+        console.log("Do something");
+        break;
+}
---
+++ week-1/practice-loop.js
@@ -0,0 +1,33 @@
+//a short for loop to count the number from one to five.
+for (i = 1; i < 6; i++) {
+    console.log(i)
+}
+console.log('Counting completed!');
+
+
+
+//a short for loop to count down the number from five to one.
+for (i = 5; i > 0; i--) {
+    console.log(i)
+}
+console.log('Countdown finished!');
+
+
+
+//a short while loop to count the number from one to five.
+var i = 1;
+while (i < 6) {
+    console.log(i);
+    i++;
+}
+console.log('Counting completed!');
+
+
+
+//a short while loop to count down the number from five to one.
+var i = 5;
+while (i > 0) {
+    console.log(i);
+    i--;
+}
+console.log('Countdown finished!');
