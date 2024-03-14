57201a65ee4908feb394559aaf3f5899d32a47e1
Naomi Zhong <xoxizhong@gmail.com>
2024-03-14 14:39:18 -0400

    loop exercise

--- week-1/practice-loop.js
+++ week-1/practice-loop.js
@@ -31,3 +31,21 @@ while (i > 0) {
     i--;
 }
 console.log('Countdown finished!');
+
+
+// a for loop excercise
+
+for (var i = 1; i < 11; i++) {
+    if (i == 1) {
+        console.log("Gold medal");
+    }
+    else if (i == 2) {
+        console.log("Silver medal");
+    }
+    else if (i == 3) {
+        console.log("Bronze medal");
+    }
+    else {
+        console.log(i);
+    }
+}
