fdbbd50c21bb0ed155d291b745bda0b33746785a
Naomi Zhong <xoxizhong@gmail.com>
2024-03-19 17:57:26 -0400

    defensive programming

--- week-2/practice-errorHandling.js
+++ week-2/practice-errorHandling.js
@@ -1,4 +1,4 @@
-
+/*practice on error handling
 function addTwoNums(a, b) {
     try {
         if (typeof(a) != "number") {
@@ -19,3 +19,27 @@ function addTwoNums(a, b) {
 
 addTwoNums(5, "5");
 console.log("It still works.")
+*/
+
+
+//practice on defensive programming: transform the following code as required.
+
+function letterFinder(word, match) {
+    var condition1 = typeof(word) == 'string' && word.length >= 2;
+    var condition2 = typeof(match) == 'string' && match.length == 1;
+    if(condition1 == true && condition2 == true) {
+    for(var i = 0; i < word.length; i++) {
+        if(word[i] == match) {
+            //if the current character at position i in the word is equal to the match
+            console.log('Found the', match, 'at', i)
+        } else {
+            console.log('---No match found at', i)
+        }
+    }
+    } else {
+        console.log("please pass correct arguments to the function")
+    }
+}
+letterFinder(1,2);
+letterFinder("cat", "c");
+
