26418dec10d7686e16d6ddb0795e2a38d9290330
Naomi Zhong <xoxizhong@gmail.com>
2024-03-25 14:40:14 -0400

    defaultParams

--- week-3/examples-class.js
+++ week-3/examples-class.js
@@ -74,3 +74,32 @@ var myTrain2 = new Train('blue', false);
 var highSpeed1 = new HighSpeedTrain(200, false, 'green', false);
 myTrain2.getPrototype();
 highSpeed1.toggleLights();
+
+//step 4: using class instance as another class' constructor's property
+class StationaryBike {
+    constructor(position, gears) {
+        this.position = position
+        this.gears = gears
+    }
+}
+
+class Treadmill {
+    constructor(position, modes) {
+        this.position = position
+        this.modes = modes
+    }
+}
+
+class Gym {
+    constructor(openHrs, stationaryBikePos, treadmillPos) {
+        this.openHrs = openHrs
+        this.stationaryBike = new StationaryBike(stationaryBikePos, 8)
+        this.treadmill = new Treadmill(treadmillPos, 5)
+    }
+}
+
+var boxingGym = new Gym('7-22', 'right corner', 'left corner')
+
+console.log(boxingGym.openHrs)
+console.log(boxingGym.stationaryBike)
+console.log(boxingGym.treadmill)
