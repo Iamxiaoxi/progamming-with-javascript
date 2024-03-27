c870a3272419511b719b091335b6ed8a7b7ace34
Naomi Zhong <xoxizhong@gmail.com>
2024-03-27 15:32:38 -0400

    domManipulation

---
+++ week-3/examples-domManipulation.js
@@ -0,0 +1,6 @@
+const h2 = document.createElement('h2')
+h2.innerText = "This is an h2 heading"
+h2.setAttribute('id', 'sub-heading')
+h2.setAttribute('class', 'secondary')
+h2;
+document.body.appendChild(h2)
