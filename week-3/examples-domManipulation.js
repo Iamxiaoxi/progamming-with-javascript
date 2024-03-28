239393b9416b1f6c35e514e6fd7a792628e037dc
Naomi Zhong <xoxizhong@gmail.com>
2024-03-28 15:54:37 -0400

    captureData

--- week-3/examples-domManipulation.js
+++ week-3/examples-domManipulation.js
@@ -1,6 +1,13 @@
+//The DOM is the model of the HTML file 
+//saved as a JavaScript object in your browser's memory.
+//The browser automatically builds the DOM for every webpage that it downloads. 
+//For example, if you type a URL into your browser's address bar, 
+//the browser would fetch the webpage that exists on this domai
 const h2 = document.createElement('h2')
 h2.innerText = "This is an h2 heading"
 h2.setAttribute('id', 'sub-heading')
 h2.setAttribute('class', 'secondary')
 h2;
 document.body.appendChild(h2)
+
+//gui
